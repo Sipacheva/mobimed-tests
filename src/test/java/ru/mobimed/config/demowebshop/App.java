5870b31c09ffcb5be6ca9155b24a5177435e91d1
Sipacheva <so.888@mail.ru>
2021-12-07 06:30:47 +0300

    added tests

---
+++ src/test/java/ru/mobimed/config/demowebshop/App.java
@@ -0,0 +1,7 @@
+package ru.mobimed.config.demowebshop;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class App {
+    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
+}
---
+++ src/test/java/ru/mobimed/tests/TestBase.java
@@ -0,0 +1,39 @@
+package ru.mobimed.tests;
+
+import ru.mobimed.config.Project;
+import ru.mobimed.helpers.AllureAttachments;
+import ru.mobimed.helpers.DriverSettings;
+import ru.mobimed.helpers.DriverUtils;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DriverSettings.configure();
+    }
+
+    @AfterEach
+    public void addAttachments() {
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+//        AllureAttachments.attachNetwork(); // todo
+        AllureAttachments.addBrowserConsoleLogs();
+
+        Selenide.closeWebDriver();
+
+        if (Project.isVideoOn()) {
+            AllureAttachments.addVideo(sessionId);
+        }
+    }
+}
